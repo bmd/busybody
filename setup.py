a684ecbc8a0d86c168bae908e3b8d7a70a2f1444
Brendan Maione-Downing <bmdowning@bluestatedigital.com>
2015-04-14 23:32:36 -0400

    Update package config files

---
+++ setup.py
@@ -0,0 +1,36 @@
+"""
+BusyBody
+--------------
+
+A library for enriching user data using the FullContact API.
+"""
+from setuptools import setup
+
+with open('README.md', 'r') as inf:
+    long_description = inf.read()
+
+setup(
+    name = 'busybody',
+    version = '0.0.1',
+    description = 'Tools for managing and enriching user data with the FullContact API',
+    long_description = long_description,
+    url = 'https://github.com/bmd/busybody',
+    author = 'Brendan Maione-Downing',
+    author_email = 'b.maionedowning@gmail.com',
+    license = 'MIT',
+    packages = ['busybody'],
+    zip_safe = False,
+    platforms = 'any',
+    include_package_data = True,
+    install_requires = ['requests', 'peewee'],
+    tests_require = ['nose>=1.0'],
+    test_suite = 'nose.collector',
+    classifiers = [
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
