79439059bf09d36876dfd1f8e8f02870ab7690aa
Brendan Maione-Downing <bmdowning@bluestatedigital.com>
2015-04-16 17:22:19 -0400

    Productionalize db setup script

--- initialize_database.py
+++ initialize_database.py
@@ -1,15 +1,30 @@
 import os
+import argparse
 
 from models import *
+from colors import Colors
 
 
 if __name__ == '__main__':
-    # avoid conflicts with existing database
-    if os.path.exists('busybody.sqlite'):
-        print 'The busybody database is already initialized.\nDelete or move it to initialize a new copy.'
+    # parse command-line arguments
+    parser = argparse.ArgumentParser(description='Initialize a busybody project database')
+    parser.add_argument('-f', '--force', action='store_true', help='Remove an existing database ')
+    args = parser.parse_args()
+
+    # check whether we can safely configure the database
+    print Colors.BOLD + '\nConfiguring BusyBody database...' + Colors.ENDC
+    print Colors.OKBLUE + '  Checking for existing database...' + Colors.ENDC
+
+    # execute database setup
+    if os.path.exists('busybody.sqlite') and not args.force:
+        print Colors.FAIL + '    FAILURE: ' + Colors.ENDC + 'The busybody database is already initialized. Run this script with -f to force database setup'
+        print Colors.FAIL + 'FAILED\n' + Colors.ENDC
     else:
-        print 'Creating busybody database'
-        # create tables specified in 'models.py'
+        if os.path.exists('busybody.sqlite'):
+            print Colors.WARNING + '    WARNING: ' + Colors.ENDC + 'An existing busybody database will be overwritten'
+            os.remove('busybody.sqlite')
+
+        print '  Creating busybody database'
         db.connect()
         db.create_tables([
             User,
@@ -20,5 +35,5 @@ if __name__ == '__main__':
             UserOrganization,
             UserModelScore
         ])
-
-        print 'Initialized Tables'
+        print Colors.OKGREEN + '    SUCCESS: ' + Colors.ENDC + 'Initialized busybody database'
+        print Colors.OKGREEN + 'DONE\n' + Colors.ENDC
